7861e7df5f1d8ad9a0dd6089bfc3507f527834ac
Samy MESSAL <smessal@e1r7p5.clusters.42paris.fr>
2022-09-02 21:23:54 +0200

    test mlx

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: smessal <smessal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 12:47:31 by smessal           #+#    #+#             */
-/*   Updated: 2022/09/02 19:45:53 by smessal          ###   ########.fr       */
+/*   Updated: 2022/09/02 21:23:01 by smessal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "mlx_linux/mlx.h"
 #include <stdio.h>
 #include "includes/so_long.h"
+#include <X11/keysym.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -22,6 +23,19 @@ typedef struct	s_data {
 	int		line_length;
 	int		endian;
 }				t_data;
+
+int	handle_no_event(void *data)
+{
+	/* This function needs to exist, but it is useless for the moment */
+	return (0);
+}
+
+int	handle_input(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->img, data->addr);
+	return (0);
+}
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -45,12 +59,10 @@ int main(void)
 	win_ptr = mlx_new_window(mlx_ptr, 512, 512, "Rvrignon zamel");
 	img.img = mlx_new_image(mlx_ptr, 512, 512);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	// while (x < 512)
-	// {
-		
-	// }
 	mlx_put_image_to_window(mlx_ptr, win_ptr, img.img, 0, 0);
 	mlx_loop(mlx_ptr);
+	mlx_loop_hook(img.img, &handle_no_event, &img);
+	mlx_key_hook(img.img, &handle_input, &img);
 	mlx_destroy_window(mlx_ptr, win_ptr);
 	mlx_destroy_display(mlx_ptr);
 	free(mlx_ptr);
