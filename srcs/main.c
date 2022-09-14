ae12df13b2f9be90c0ab40ab1a2b78ee60005952
samymessal <samy.messal.sm@gmail.com>
2022-09-14 20:05:07 +0200

    main adjusted

--- srcs/main.c
+++ srcs/main.c
@@ -6,26 +6,27 @@
 /*   By: smessal <smessal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 16:20:55 by smessal           #+#    #+#             */
-/*   Updated: 2022/09/11 16:51:34 by smessal          ###   ########.fr       */
+/*   Updated: 2022/09/14 20:04:56 by smessal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "../includes/so_long.h"
 
 int	main(int ac, char **av)
 {
 	char **map;
 	
 	if (ac != 2)
+	{
+		write(1, "Error\nToo many arguments\n", 25);
 		return (0);
+	}
 	map = read_map(av[1]);
-	// if (!map)
-	// {
-	// 	write(1, "Error\nFailed map allocation", 28);
-	// 	return (0);
-	// }
-	// if (!messages(av, map))
-	// 	return (0);
+	if (!map)
+	{
+		write(1, "Error\nFailed map allocation", 28);
+		return (0);
+	}
+	if (!messages(av, map))
+		return (0);
 	init_win(av[1], map);
 	return (0);
 }
